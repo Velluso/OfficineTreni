baf802ed77ad9cf7c38d07ecb83550fc19731d70
Federer97 <83187582+Federer97@users.noreply.github.com>
2023-07-28 08:42:09 +0200

    Update OrdineDAOImpl.java

--- src 26.07 con autenticazione/main/java/dao/OrdineDAOImpl.java
+++ src 26.07 con autenticazione/main/java/dao/OrdineDAOImpl.java
@@ -1,9 +1,29 @@
 package dao;
 
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 import bean.Ordine;
 
 public class OrdineDAOImpl extends BaseDAO implements OrdineDAO {
 
+    private static SessionFactory factory; 
+	static {
+		try {
+			factory = new Configuration().configure().buildSessionFactory();
+		} catch (Throwable ex) { 
+			System.err.println("Failed to create sessionFactory object." + ex);
+			throw new ExceptionInInitializerError(ex); 
+		}
+	}
+    
     public Object create(Ordine bean){
         return super.create(bean);
     }
@@ -16,8 +36,26 @@ public class OrdineDAOImpl extends BaseDAO implements OrdineDAO {
         super.update(bean);
     }
 
-    public Ordine find(Integer id) {
-        return (Ordine) super.find(Ordine.class,id);
+    public List<Ordine> find(String username) {
+    	
+    	Session session = factory.openSession();
+	    Transaction tx = null;
+	    List<Ordine> results=null;
+	    try {
+	    	tx=session.beginTransaction();
+	    	TypedQuery<Ordine> query = session.getNamedQuery("ordiniDatoUsername");    
+            query.setParameter("username",username);   
+            results=query.getResultList(); 
+            System.out.println(results);
+	    	tx.commit();
+	    } catch (HibernateException e) {
+	    	if (tx!=null) tx.rollback();
+	        e.printStackTrace(); 
+	    } finally {
+	    	session.close(); 
+	    }
+    	
+        return results;
     }
 
 
